7b1030aaee924f6d1a8cc663d70ce5e9f085c64c
Mayur <thisispatil@gmail.com>
2021-09-22 22:16:58 +0530

    add(contract): todolist:
    - add state variable

---
+++ contracts/TodoList.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.5.0;
+
+contract TodoList {
+    // state variable
+    // these are like class variables
+    // they represent the state of the application at the moment
+    // public: solidity keyword exposes a getter
+    uint public taskCount = 0;
+}
